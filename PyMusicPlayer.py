2b39b4313324aaa1574d2864ea2e8b094b20d986
Arrokoth <arrokoth233@gmail.com>
2022-06-02 15:26:35 +0200

    Just a push

---
+++ PyMusicPlayer.py
@@ -0,0 +1,4 @@
+import pyMusicPlayer
+
+if __name__ == '__main__':
+    x = pyMusicPlayer.Application()
---
+++ pyMusicPlayer/__init__.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+class Application:
+
+    def __init__(self):
+        frame = Tk()
+        frame.geometry("320x528")
+        frame.minsize(320, 528)
+        frame.maxsize(375, 612)
+        frame.mainloop()
